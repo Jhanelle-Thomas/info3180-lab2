9109598e5eb3b8cb766600355b4d03d9919b8fc1
Jhanelle Thomas <jhanelle.thomas02@mymona.uwi.edu>
2017-02-06 03:45:01 +0000

    added profile template and css

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for
+import time
 
 
 ###
@@ -23,6 +24,12 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+    
+    
+@app.route('/profile')
+def profile():
+    """Render the profile page."""
+    return render_template('profile.html', time = timeinfo())
 
 
 ###
@@ -51,6 +58,14 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+###
+# Returns the date formated as Day, Date Month Year (e.g. Wed, 1 Feb 2017)
+###
+
+def timeinfo():
+    return time.strftime("%a, %d %b %Y")
+
 
 
 if __name__ == '__main__':
